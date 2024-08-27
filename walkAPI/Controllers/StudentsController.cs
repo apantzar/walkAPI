aa88d34bc0c552a1527d48d6726a0c904fc23990
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-08-27 18:27:47 +0300

    Add project files.

---
+++ walkAPI/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace walkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[] { "John", "Jane", "Mark", "David" };
+            return Ok(studentsNames);
+        }
+    }
+}
